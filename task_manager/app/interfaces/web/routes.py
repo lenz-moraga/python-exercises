2be2fe3d281a650001d7aab2821ba2c83c876d61
lenz-moraga <49219425+lenz-moraga@users.noreply.github.com>
2025-08-11 16:13:36 -0600

    implement flask to list the tasks in a web ui

--- task_manager/app/interfaces/web/routes.py
+++ task_manager/app/interfaces/web/routes.py
@@ -1,11 +1,32 @@
-# app/web/routes.py
-from flask import Blueprint, render_template
-from ...domain.services.task_service import TaskService
+from flask import render_template, request, redirect, url_for
+from app.domain.services.task_service import TaskService
+from app.infrastructure.repositores.task_repository import TaskRespository
 
-bp = Blueprint("web", __name__)
-service = TaskService()
 
-@bp.route("/")
-def index():
-    tasks = service.list_tasks()
-    return render_template("tasks.html", tasks=tasks)
+repository = TaskRespository()
+service = TaskService(repository)
+
+
+def register_routes(app):
+
+    @app.route('/')
+    def index():
+        tasks = service.list_tasks(sort_by="priority")
+        return render_template('index.html', tasks=tasks)
+
+    @app.route('/add', methods=['POST'])
+    def add_task():
+        title = request.form.get('title')
+        priority = int(request.form.get('priority', 3))
+        service.add_task(title, priority)
+        return redirect(url_for('index'))
+
+    @app.route('/complete/<task_id>')
+    def complete_task(task_id):
+        service.complete_task(task_id)
+        return redirect(url_for('index'))
+
+    @app.route('/uncomplete/<task_id>')
+    def uncomplete_task(task_id):
+        service.uncomplete_task(task_id)
+        return redirect(url_for('index'))
